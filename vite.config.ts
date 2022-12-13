d385ff3f811c0c12574561cf98e4f371a8e025aa
Divya Yashaswi Kanney <49753870+divyaYK@users.noreply.github.com>
2022-12-13 13:37:58 +0530

    setup: mistakes corrected

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import path from "path";
+import EsLint from "vite-plugin-linter";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+const { EsLinter, linterPlugin } = EsLint;
+import { peerDependencies } from "./package.json";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ["./src}/**/*.{ts,tsx}"],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "ReactComponents",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-components.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+}));
