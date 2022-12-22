56bc74d671cade3d0d5c2ecdaca4e644b171cd5e
Divya Yashaswi Kanney <49753870+divyaYK@users.noreply.github.com>
2022-12-22 13:36:50 +0530

    chore: stale comments added

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,19 @@
  * @overview exports all components created as part of this library
  */
 
+/**
+ * Components
+ */
 export * from "./components/atoms";
 export * from "./components/molecules";
 export * from "./components/organisms";
 
+/**
+ * Hooks
+ */
 export * from "./hooks/useUniqueId";
 
+/**
+ * Theme configuration
+ */
 export * from "./shared/theme";
